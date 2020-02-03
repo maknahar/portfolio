34f702ac060f66388db817d5676183c2fcb68dd9
Ryan Fitzgerald <me@ryanfitzgerald.ca>
2020-02-02 20:16:01 -0500

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Full-Stack Developer`,
+    description: `Full-Stack Developer based in Toronto, Ontario, Canada.`,
+    author: `@RyanAFitzgerald`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
