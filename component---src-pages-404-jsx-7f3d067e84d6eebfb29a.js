f2ee57d851bef066d42a4baf8eb18d66ac855af1
maknahar <mayankp@live.in>
2021-06-27 18:13:16 +0530

    Updates

---
+++ component---src-pages-404-jsx-7f3d067e84d6eebfb29a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{pssB:function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),u=n.n(a),o=n("6uTu"),l=n("ivnd");e.default=function(){return u.a.createElement(o.a,null,u.a.createElement(l.a,{title:"404: Not found"}),u.a.createElement("h1",null,"404: Not Found"),u.a.createElement("p",null,"You just hit a route that doesn't exist."))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-7f3d067e84d6eebfb29a.js.map
